954056aa99af446fa975adc5e21a7a50d59b4e06
Matteo Crippa <matteocrippa@github.users.com>
2024-01-07 00:22:17 +0100

    fix crash in current when clean is true

--- src/current.cpp
+++ src/current.cpp
@@ -71,7 +71,10 @@ void current_setJob(const Notification &notification)
     if (notification.clean_jobs)
     {
         current_job_processed = 0;
-        l_debug(TAG_CURRENT, "Job: %s is cleaned and replaced with %s", current_job->job_id.c_str(), notification.job_id.c_str());
+        if (current_job != nullptr)
+        {
+          l_debug(TAG_CURRENT, "Job: %s is cleaned and replaced with %s", current_job->job_id.c_str(), notification.job_id.c_str());
+        }
     }
     if (current_job_is_valid == 1)
     {
