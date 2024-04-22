2a362049abaf7aa7228f65e8620853f11953f63b
Matteo Crippa <matteocrippa@github.users.com>
2024-04-22 21:54:49 +0200

    fixes setup and configuration

--- src/model/configuration.h
+++ src/model/configuration.h
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include "utils/log.h"
 
+#define TAG_CONFIGURATION "Configuration"
+
 struct Configuration
 {
     std::string wifi_ssid = "";
@@ -21,17 +23,17 @@ struct Configuration
 
     void print()
     {
-        l_info("wifi_ssid: %s", wifi_ssid.c_str());
-        l_info("wifi_password: %s", wifi_password.c_str());
-        l_info("wallet_address: %s", wallet_address.c_str());
-        l_info("pool_password: %s", pool_password.c_str());
-        l_info("pool_url: %s", pool_url.c_str());
-        l_info("pool_port: %d", std::to_string(pool_port).c_str());
-        l_info("blink_enabled: %s", blink_enabled.c_str());
-        l_info("blink_brightness: %s", std::to_string(blink_brightness).c_str());
-        l_info("lcd_on_start: %s", lcd_on_start.c_str());
-        l_info("miner_type: %s", miner_type.c_str());
-        l_info("auto_update: %s", auto_update.c_str());
+        l_info(TAG_CONFIGURATION, "wifi_ssid: %s", wifi_ssid.c_str());
+        l_info(TAG_CONFIGURATION, "wifi_password: %s", wifi_password.c_str());
+        l_info(TAG_CONFIGURATION, "wallet_address: %s", wallet_address.c_str());
+        l_info(TAG_CONFIGURATION, "pool_password: %s", pool_password.c_str());
+        l_info(TAG_CONFIGURATION, "pool_url: %s", pool_url.c_str());
+        l_info(TAG_CONFIGURATION, "pool_port: %d", std::to_string(pool_port).c_str());
+        l_info(TAG_CONFIGURATION, "blink_enabled: %s", blink_enabled.c_str());
+        l_info(TAG_CONFIGURATION, "blink_brightness: %s", std::to_string(blink_brightness).c_str());
+        l_info(TAG_CONFIGURATION, "lcd_on_start: %s", lcd_on_start.c_str());
+        l_info(TAG_CONFIGURATION, "miner_type: %s", miner_type.c_str());
+        l_info(TAG_CONFIGURATION, "auto_update: %s", auto_update.c_str());
     }
 };
 
